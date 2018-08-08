470dd59e68cad7e84c92974ae3bb099e823e7d86
HBahaa <hebabahaa2208@yahoo.com>
2018-08-08 12:41:48 +0200

    school updated

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -30,11 +30,11 @@ export class AppComponent {
 
 		// this.authService.isLoggedIn.subscribe((isLoggedIn:boolean)=>{
 		// 	if (!isLoggedIn) {
-				this.router.navigate(['/teacher/profile']);
+				this.router.navigate(['/nahj/']);
 			// }
 			// else{
 				this.authService.userType.subscribe(type=>{
-					this.type = 2;
+					this.type = 1;
 				})
 		// 	}
 		// })
