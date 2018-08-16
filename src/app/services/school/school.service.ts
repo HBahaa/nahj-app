a83296897f866032b5798a7f7348f2455eb3ab9d
ezamlee <a.m.essam1992@gmail.com>
2018-08-16 18:14:37 +0200

    school CRUD

--- src/app/services/school/school.service.ts
+++ src/app/services/school/school.service.ts
@@ -13,50 +13,178 @@ export class SchoolService {
     console.log(config)
     switch(config.method){
       case "POST" : //update
-        query = `mutation ($address: String!, $gps: String!, $phone: String!, $fax: String!, $district: String!, $adminNum: Int!, $studentsNum: Int!, $classesNum: Int!, $teachersNum: Int!, $StudyYears: String!, $lowestStudyYear: String!, $highestStudyYear: String!, $name: String!, $motherComp: String!, $Id: ID!, $admin: NahjAdminUpdateDataInput!, $adminId: ID!) { updateSchool(data: {address: $address, gps: $gps, phone: $phone, fax: $fax, district: $district, adminNum: $adminNum, studentsNum: $studentsNum, classesNum: $classesNum, teachersNum: $teachersNum, StudyYears: $StudyYears, lowestStudyYear: $lowestStudyYear, highestStudyYear: $highestStudyYear, name: $name, motherComp: $motherComp, admin: {update: {where: {id: $adminId}, data: $admin}}}, where: {id: $Id}) { id address admin { id name job type phone whatsApp email username password } gps phone } }  ` 
+        query = `mutation ($admin: NahjAdminUpdateDataInput!, $adminId: ID!, $adminRes: NahjAdminUpdateDataInput!, $adminResID: ID!, $address: String!, $gps: String!, $phone: String!, $fax: String!, $district: String!, $adminNum: Int!, $studentsNum: Int!, $classesNum: Int!, $teachersNum: Int!, $StudyYears: String!, $lowestStudyYear: String!, $highestStudyYear: String!, $name: String!, $motherComp: String!, $level1Id: ID!) {
+          updateSchool(data: {admin: {update: [{where: {id: $adminId}, data: $admin}, {where: {id: $adminResID}, data: $adminRes}]}, address: $address, gps: $gps, phone: $phone, fax: $fax, district: $district, adminNum: $adminNum, studentsNum: $studentsNum, classesNum: $classesNum, teachersNum: $teachersNum, StudyYears: $StudyYears, lowestStudyYear: $lowestStudyYear, highestStudyYear: $highestStudyYear, name: $name, motherComp: $motherComp, levels: {connect: {id: $level1Id}}}, where: {id: ""}) {
+            id
+            address
+            admin {
+              id
+              name
+              job
+              type
+              phone
+              whatsApp
+              email
+              username
+              password
+            }
+            gps
+            phone
+            fax
+            district
+            adminNum
+            studentsNum
+            classesNum
+            teachersNum
+            StudyYears
+            lowestStudyYear
+            highestStudyYear
+            name
+            motherComp
+            levels {
+              id
+              name
+              LevelTwo {
+                id
+                name
+                levelThree {
+                  id
+                  name
+                }
+              }
+            }
+          }
+        }
+        ` 
         variable = {
-          id:config.id,
           address:config.address,
+          adminRes:config.adminRes,
           admin:config.admin,
           gps:config.gps,
-          phone:config.phone,
+          phone:config.gps,
           fax:config.fax,
           district:config.district,
           adminNum:config.adminNum,
           studentsNum:config.studentsNum,
-          classesNum:config.classesNum,
           teachersNum:config.teachersNum,
+          classesNum:config.classesNum,
           StudyYears:config.StudyYears,
           lowestStudyYear:config.lowestStudyYear,
           highestStudyYear:config.highestStudyYear,
           name:config.name,
           motherComp:config.motherComp,
-          adminId:config.adminId
+          level1Id:config.level1Id,
+          adminId: config.adminId,
+  				adminResID: config.adminResID
         }       
       break;  
       case "GET": //read
-        query = `{schools { id address admin { id name job type phone whatsApp email username password } gps phone fax district adminNum studentsNum classesNum teachersNum StudyYears lowestStudyYear highestStudyYear name motherComp } }`;
+        query = `{
+          schools {
+            id
+            address
+            admin {
+              id
+              name
+              job
+              type
+              phone
+              whatsApp
+              email
+              username
+              password
+            }
+            gps
+            phone
+            fax
+            district
+            adminNum
+            studentsNum
+            classesNum
+            teachersNum
+            StudyYears
+            lowestStudyYear
+            highestStudyYear
+            name
+            motherComp
+            levels {
+              id
+              name
+              LevelTwo {
+                id
+                name
+                levelThree {
+                  id
+                  name
+                }
+              }
+            }
+          }
+        }
+        `;
       break;
       case "PUT"://create
-        query = `mutation ($admin: NahjAdminCreateInput!, $address: String!, $gps: String!, $phone: String!, $fax: String!, $district: String!, $adminNum: Int!, $studentsNum: Int!, $classesNum: Int!, $teachersNum: Int!, $StudyYears: String!, $lowestStudyYear: String!, $highestStudyYear: String!, $name: String!, $motherComp: String!) { createSchool(data: {address: $address, gps: $gps, phone: $phone, fax: $fax, district: $district, adminNum: $adminNum, studentsNum: $studentsNum, classesNum: $classesNum, teachersNum: $teachersNum, StudyYears: $StudyYears, lowestStudyYear: $lowestStudyYear, highestStudyYear: $highestStudyYear, name: $name, motherComp: $motherComp, admin: {create: [$admin]}}) { id address admin { id name job type phone whatsApp email username password } gps phone fax district adminNum studentsNum classesNum teachersNum StudyYears lowestStudyYear highestStudyYear name motherComp } }`
+        query = `mutation ($admin: NahjAdminCreateInput!, $adminRes: NahjAdminCreateInput!, $address: String!, $gps: String!, $phone: String!, $fax: String!, $district: String!, $adminNum: Int!, $studentsNum: Int!, $classesNum: Int!, $teachersNum: Int!, $StudyYears: String!, $lowestStudyYear: String!, $highestStudyYear: String!, $name: String!, $motherComp: String!, $level1Id: ID!) {
+          createSchool(data: {admin: {create: [$admin, $adminRes]}, address: $address, gps: $gps, phone: $phone, fax: $fax, district: $district, adminNum: $adminNum, studentsNum: $studentsNum, classesNum: $classesNum, teachersNum: $teachersNum, StudyYears: $StudyYears, lowestStudyYear: $lowestStudyYear, highestStudyYear: $highestStudyYear, name: $name, motherComp: $motherComp, levels: {connect: {id: $level1Id}}}) {
+            id
+            address
+            admin {
+              id
+              name
+              job
+              type
+              phone
+              whatsApp
+              email
+              username
+              password
+            }
+            gps
+            phone
+            fax
+            district
+            adminNum
+            studentsNum
+            classesNum
+            teachersNum
+            StudyYears
+            lowestStudyYear
+            highestStudyYear
+            name
+            motherComp
+            levels {
+              id
+              name
+              LevelTwo {
+                id
+                name
+                levelThree {
+                  id
+                  name
+                }
+              }
+            }
+          }
+        }
+        `
         variable = {
-          email	:config.email,
-          id:config.id,
           address:config.address,
+          adminRes:config.adminRes,
           admin:config.admin,
           gps:config.gps,
-          phone:config.phone,
+          phone:config.gps,
           fax:config.fax,
           district:config.district,
           adminNum:config.adminNum,
           studentsNum:config.studentsNum,
-          classesNum:config.classesNum,
           teachersNum:config.teachersNum,
+          classesNum:config.classesNum,
           StudyYears:config.StudyYears,
           lowestStudyYear:config.lowestStudyYear,
           highestStudyYear:config.highestStudyYear,
           name:config.name,
-          motherComp:config.motherComp
+          motherComp:config.motherComp,
+          level1Id:config.level1Id,
         }
 
       break;
