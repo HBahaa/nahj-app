ef66f382bb0a76fe0f3947edc8097012a8030491
ezamlee <a.m.essam1992@gmail.com>
2018-09-07 16:06:16 +0200

    updated study level services

---
+++ src/app/services/schoolAdmin/studyLevelsOne.ts
@@ -0,0 +1,97 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+    providedIn: 'root'
+})
+export class studyLevelsOne {
+
+    constructor(private http: HttpClient) { }
+    fields = ['schoolId','levelOneName','levelOneId'];
+    createVariable(config){
+        let newVar = {};
+        for (let key in config){
+            if(this.fields.includes(key) && config[key])
+                newVar[key] = config[key]
+        }
+        return newVar
+    }
+    service(config) {
+        let query: string = ``
+        let variable: object = {};
+        switch (config.method) {
+            case "POST": //update
+                query=`mutation(
+                    $levelOneName: String!
+                    $levelOneId:ID!
+                  ){
+                    updateStudyLevelOne(
+                      data:{
+                        name:$levelOneName
+                      }
+                      where:{
+                        id:$levelOneId
+                      }
+                    ){
+                      id
+                    }
+                  }`
+                  variable = this.createVariable(config);
+            break;
+            case "PUT" : //Create
+                query=`mutation($levelOneName: String!, $schoolId: ID!) {
+                    updateSchool(
+                      data: {
+                        specificStudyLevels: {
+                          create: { studyLevelOne: { create: { name: $levelOneName } } }
+                        }
+                      }
+                      where: { id: $schoolId }
+                    ) {
+                      id
+                    }
+                  }
+                `
+                variable = this.createVariable(config);
+            break;
+            case "DELETE" : //Delete
+                  query=`mutation($levelOneId: ID!) {
+                    deleteStudyLevelOne(where: { id: $levelOneId }) {
+                      id
+                    }
+                  }
+                  `
+                  variable = this.createVariable(config);
+            break;
+            case "GET":
+                  query=`query($schoolId: ID!) {
+                    school(where: { id: $schoolId }) {
+                      specificStudyLevels {
+                        id
+                        studyLevelOne {
+                          id
+                          name
+                          studyLevelTwo {
+                            id
+                            name
+                            studylevelThree {
+                              id
+                              name
+                            }
+                          }
+                        }
+                      }
+                    }
+                  }
+                  `
+            break;
+        }
+
+        return this
+            .http
+            .post(`${config.url}`, {
+                "query": query,
+                "variables": variable
+            });
+    }
+}
