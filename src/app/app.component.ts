4446bb80d940c296220d726702f08a28d2226673
HBahaa <hebabahaa2208@yahoo.com>
2018-10-08 07:09:18 +0200

    login page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -22,28 +22,28 @@ export class AppComponent {
 
 	async ngOnInit() {
 		this.isLoggedIn$ = this.authService.isLoggedIn;
-		// this.authService.isLoggedIn.subscribe((isLoggedIn:boolean)=>{
-		// 	if (!isLoggedIn) {
-		// 		this.router.navigate(['/login']);
-		// 	}
-		// 	else if (isLoggedIn) {
-		// 		this.authService.userType.subscribe(type=>{
-		// 			this.type = type;
-		// 		})
-		// 	}
-		// })
 		this.authService.isLoggedIn.subscribe((isLoggedIn:boolean)=>{
-			// if (!isLoggedIn) {
-			// 	this.router.navigate(['/login']);
-			// }
-			// else 
-			if (isLoggedIn) {
+			if (!isLoggedIn) {
+				this.router.navigate(['/login']);
+			}
+			else if (isLoggedIn) {
 				this.authService.userType.subscribe(type=>{
-					this.router.navigate(['/school/teachers']);
-					this.type = 3;
+					this.type = type;
 				})
 			}
 		})
+		// this.authService.isLoggedIn.subscribe((isLoggedIn:boolean)=>{
+		// 	// if (!isLoggedIn) {
+		// 	// 	this.router.navigate(['/login']);
+		// 	// }
+		// 	// else 
+		// 	if (isLoggedIn) {
+		// 		this.authService.userType.subscribe(type=>{
+		// 			this.router.navigate(['/school/teachers']);
+		// 			this.type = 3;
+		// 		})
+		// 	}
+		// })
 	}
 
 }
